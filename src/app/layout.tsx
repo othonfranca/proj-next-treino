2aeca39cdba3cd43b2508157da324e4c1c8f7e44
othonfranca <othon.franca.dev@gmail.com>
2023-05-23 17:34:55 -0300

    Criar handleForm para buscar Author e embelezar tela

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-montserrat' })
+
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,74 @@
-import { UserType } from '@/interface/UserType';
-import Image from 'next/image'
+'use client'
+
+import { AllQuotes, Quote } from '@/interface/AllQuotes';
+import React, { FormEvent, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 
-export default async function Home() {
-  const user = await fetch('https://dummyjson.com/users/2')
-    .then(res => res.json())
-    .then((data) => {
-      return data as UserType
-    });
-  return <div><p>{user.firstName}</p><p>{user.company.name}</p></div>
+export default function Home() {
+
+  const [txtAuthor, setTxtAuthor] = useState("")
+  const [allQuotes, setAllQuotes] = useState<Quote[]>([])
+
+  useEffect(() => {
+    async function handleFetch() {
+      const allQuotes = await fetch('https://dummyjson.com/quotes?limit=100')
+        .then(res => res.json())
+        .then((data: AllQuotes) => {
+          return data.quotes as Quote[]
+        })
+
+      setAllQuotes(allQuotes)
+    }
+
+    handleFetch()
+  }, [])
+
+  // const singleQuote = await fetch(`https://dummyjson.com/quotes/1`)
+  //   .then(res => res.json())
+  //   .then((data) => {
+  //     return data as Quote
+  //   })
+
+
+
+  console.log(txtAuthor)
+
+
+
+  return (
+    <>
+      <form className='flex gap-4 relative p-3' >
+        <input onChange={(event) => {
+          setTxtAuthor(event.target.value)
+        }} value={txtAuthor} type="text" name="textoAutor" id="textoAutor" className='text-black p-1.5' placeholder='Digite o autor...' />
+        <button className='bg-green-400 text-black flex flex-column relative items-center p-1 rounded-md' type='submit'>Pesquisar</button>
+      </form>
+      <div className='flex flex-col p-3 items-center gap-6 bg-gray-800'>
+        {allQuotes.map((quote, index) => {
+          index = quote.id
+          return (
+            <div key={index} className='flex flex-col gap-1 max-w-fit bg-gray-700 rounded-md'><p className='p-2'>"{quote.quote}"</p>
+              <p className='pl-2 pb-2'>{quote.author}</p>
+              <div className='flex flex-col pr-4 pb-4'><button className='bg-green-400 text-black flex flex-column relative items-center self-end p-2 rounded-full'>Favoritar</button></div></div>
+          )
+
+        })}
+      </div>
+    </>
+  )
+
 }
+
+// <div className='p-3 flex flex-col justify-center bg-gray-500'>
+// {allQuotes.quotes.map((quote) => {
+//   return <div key={quote.id}>{quote.author}</div>
+// })}
+// </div>
+
+
+
+
+
+
+
